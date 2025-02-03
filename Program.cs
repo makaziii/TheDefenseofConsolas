78584a523cb2e4f2dce581bf2306c2254e8a5f67
Janat Kazibwe <janat.kazibwe@mnsu.edu>
2025-02-02 22:35:30 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class DefenseOfConsolas
+{
+    static void Main()
+    {
+        System.Console.Title = "Defense of Consolas";
+
+        //Ask for user input
+        System.Console.Write("Target Row? ");
+        int targetRow = Convert.ToInt32(System.Console.ReadLine());
+
+        System.Console.Write("Target Column? ");
+        int targetColumn = Convert.ToInt32(System.Console.ReadLine());
+
+        // the computng of four locations
+        int left = targetColumn - 1;
+        int right = targetColumn + 1;
+        int up = targetRow - 1;
+        int down = targetRow + 1;
+
+        
+        //outputing deployment instructions
+        System.Console.WriteLine("Deploy to:");
+        System.Console.WriteLine($"({targetRow}, {left})");
+        System.Console.WriteLine($"({up}, {targetColumn})");
+        System.Console.WriteLine($"({targetRow}, {right})");
+        System.Console.WriteLine($"({down}, {targetColumn})");
+
+        
+    }
+}
